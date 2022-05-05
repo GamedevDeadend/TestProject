843dc9f524730de959d6773930bff69c4629f48a
GamedevDeadend <97394300+GamedevDeadend@users.noreply.github.com>
2022-05-06 01:02:32 +0530

    Optimized speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,32 +7,28 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rigidbody2d;
     public float speed;
 
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetAxis("Horizontal") > 0)
         {
-            rigidbody2d.velocity = new Vector2(speed*Time.deltaTime, 0f);
+            rigidbody2d.velocity = new Vector2(speed, 0f);
         }
         
         else if(Input.GetAxis("Horizontal") < 0)
         {
-            rigidbody2d.velocity = new Vector2(-speed*Time.deltaTime, 0f);
+            rigidbody2d.velocity = new Vector2(-speed, 0f);
         }
 
         else if(Input.GetAxis("Vertical") > 0)
         {
-            rigidbody2d.velocity = new Vector2(0,speed*Time.deltaTime);
+            rigidbody2d.velocity = new Vector2(0,speed);
         }
 
         else if(Input.GetAxis("Vertical") < 0)
         {
-            rigidbody2d.velocity = new Vector2(0,-speed*Time.deltaTime);
+            rigidbody2d.velocity = new Vector2(0,-speed);
         }
 
         else if (Input.GetAxis("Horizontal") == 0f && Input.GetAxis("Vertical") == 0)
